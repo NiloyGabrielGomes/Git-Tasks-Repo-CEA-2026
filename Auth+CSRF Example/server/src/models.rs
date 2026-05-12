93d3f6ce5e754ef72279dd73837a698f0f07406c
NiloyGabrielGomes <niloy.gabriel@craftsmensoftware.com>
2026-05-12 12:57:36 +0600

    feat(auth): add data models and update dependencies
    Implement authentication models including LoginRequest and JWT Claims. Also updated Cargo.toml
    by replacing logging dependencies with the time crate.

---
+++ Auth+CSRF Example/server/src/models.rs
@@ -0,0 +1,13 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize)]
+pub struct LoginRequest {
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Serialize, Clone)]
+pub struct Claims {
+    pub email: String,
+    pub exp: usize,
+}
